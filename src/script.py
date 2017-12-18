e0f4534a06a8b81f1f7757e85a565aefa3f34ba9
Dharmang Bhavsar <Dharmang Bhavsar>
2017-12-18 01:29:47 -0500

    Adding source files namely lr_model.py, bkt_model.py and script.py

---
+++ src/script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 16:26:49 2017
+
+@author: ayush
+"""
+
+import os 
+import sys
+
+os.system('python bkt_model.py')
+os.system('python lr_model.py')
